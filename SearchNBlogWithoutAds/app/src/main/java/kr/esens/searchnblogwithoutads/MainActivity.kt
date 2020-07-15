041403d1a239c1f782454df717a2c60b49d2de13
ESENS12 <ghdldid12@gmail.com>
2020-07-15 12:53:01 +0900

    add basic layout

--- SearchNBlogWithoutAds/app/src/main/java/kr/esens/searchnblogwithoutads/MainActivity.kt
+++ SearchNBlogWithoutAds/app/src/main/java/kr/esens/searchnblogwithoutads/MainActivity.kt
@@ -2,10 +2,14 @@ package kr.esens.searchnblogwithoutads
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        cl_no_data.visibility = View.INVISIBLE;
+
     }
 }
