255db2ee3907b0938644f6beeaec9f9d580f92aa
ESENS12 <ghdldid12@gmail.com>
2020-07-20 14:27:19 +0900

    Update Simple HTML Parser Example Using JSoup

--- JSoupExample/README.md
+++ JSoupExample/README.md
@@ -3,4 +3,5 @@
 
 # References
 - https://jsoup.org/
+- coroutines (https://developer.android.com/kotlin/coroutines)
 - https://m.blog.naver.com/PostView.nhn?blogId=cosmosjs&logNo=221603721844&proxyReferer=https:%2F%2Fwww.google.com%2F
--- JSoupExample/app/src/main/java/kr/esens/jsoupexample/MainActivity.kt
+++ JSoupExample/app/src/main/java/kr/esens/jsoupexample/MainActivity.kt
@@ -1,11 +1,54 @@
 package kr.esens.jsoupexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.UiThread
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import org.jsoup.Connection
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 class MainActivity : AppCompatActivity() {
+
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+    private val TAG  = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        coroutineScope.launch(Dispatchers.IO) {
+            val url : String = "https://search.naver.com/search.naver?where=post&st=sim&query=장호항&start=1&srchby=all&tab_pge=tab_pge"
+            val res : Document? = getHtmlDocument(url)
+//            Log.i(TAG , "res : $res")
+
+//            el#id ☞ id로 가지고 오기
+//            el.class ☞ class로 가지고 오기
+//            el[attr] ☞ attribute로 가지고 오기
+            val elem: Elements? = res?.select("a.sh_blog_title")
+
+            elem?.forEach {
+                Log.d(TAG,"item : ${it.attr("href")}")
+            }
+        }
+
     }
+
+    @UiThread
+    private fun getHtmlDocument(url : String): Document? {
+        val response: Connection.Response = Jsoup.connect(url)
+            .method(Connection.Method.GET)
+            .execute()
+        return response.parse();
+    }
+
 }
+
+
+
+
