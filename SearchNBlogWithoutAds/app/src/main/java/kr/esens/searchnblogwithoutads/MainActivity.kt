003a12e721a63c09b853e3f22cf2091b091a69c6
ESENS12 <ghdldid12@gmail.com>
2020-07-22 15:32:51 +0900

    Update TIL

--- SearchNBlogWithoutAds/app/src/main/java/kr/esens/searchnblogwithoutads/MainActivity.kt
+++ SearchNBlogWithoutAds/app/src/main/java/kr/esens/searchnblogwithoutads/MainActivity.kt
@@ -1,23 +1,34 @@
 package kr.esens.searchnblogwithoutads
 
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.webkit.WebViewClient
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import okhttp3.ResponseBody
+import org.jsoup.Connection
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.http.Query
 
 
 class MainActivity : AppCompatActivity() {
     companion object{
         const val TAG = "MainActivity"
     }
+
+    private val parentJob = Job()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,33 +44,62 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun executeSearch(searchQuery : String){
-        /*
-        *  임시 테스트용 변수 생성
-        *
-        * */
+
         var searchOption = "sim";
         var page = 1;
         cl_no_data.visibility = View.INVISIBLE;
 
-        SearchRetrofit.getService().requestBlogList(query = searchQuery, st=searchOption, start = page).enqueue(object :
+        SearchRetrofit.getService().requestBlogList(query = searchQuery, st = searchOption, start = page).enqueue(object :
             Callback<ResponseBody> {
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                 Log.e(TAG, "res Failure : $t");
             }
 
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                Log.d(TAG, "onResponse  : $response" );
-//                val inputStream = response.body()?.byteStream()
-//                val bitmap = BitmapFactory.decodeStream(inputStream);
-//                iv_image.setImageBitmap(bitmap);
+                val str_res : String = response.body()?.string().toString();
+
+                coroutineScope.launch(Dispatchers.IO) {
+                    Log.d(TAG, "str_res  : ${str_res.length}" );
+                    val res : Document? = Jsoup.parse(str_res)
+                    val elem: Elements? = res?.select("a.sh_blog_title")
+                    elem?.forEachIndexed() { index, it ->
+
+                        val blog_url = parseBlogUri(it.attr("href"))
+                        Log.d(TAG,"blog_url : $blog_url")
+
+                        val res_item : Document? = getImageUriFromBlog(blog_url)
+                        val elem_item : Elements? = res_item?.select("div.se-module.se-module-image > a > img[src]");
+//                        Log.d(TAG,"res_item :  ${res_item.select("div.se-module.se-module-image > a > img[src]")}")
+//                        val img_elem = getImageUriFromBlog(blog_url)
+//                        Log.d(TAG,"img_elem :$img_elem")
+                        elem_item?.forEach {
+                            Log.d(TAG,"img : ${it.attr("src")}");
+                        }
+                        Log.d(TAG,"====================$index=======================")
+                    }
+                }
             }
         })
+    }
 
-        //webview test
-//        my_webview.visibility = View.VISIBLE;
-//        my_webview.loadUrl("https://search.naver.com/search.naver?query=${searchQuery}&sm=tab_pge&srchby=all&st=${searchOption}&where=post&start=${page}")
+    @UiThread
+    private fun getImageUriFromBlog(url : String): Document? {
+        val response: Connection.Response = Jsoup.connect(url)
+            .method(Connection.Method.GET)
+            .execute()
+//        return response.parse().select("img")
+        return response.parse()
+    }
 
-
-
+    //blog.me로 끝나는 naver blog url을 blog.naver.com으로 변경해줘야함
+    private fun parseBlogUri(url : String) : String {
+        if (url.indexOf("blog.me") > -1) {
+            val nickName = url.substring(url.indexOf("/") + 2, url.indexOf("."))
+            val postNumber = url.substring(url.lastIndexOf("/") + 1)
+            // https://m.blog.naver.com/conanronse?Redirect=Log&logNo=221607525803
+            return "https://m.blog.naver.com/${nickName}?Redirect=Log&logNo=${postNumber}"
+        } else {
+            return url.replace("https://", "https://m.")
+        }
     }
 }
