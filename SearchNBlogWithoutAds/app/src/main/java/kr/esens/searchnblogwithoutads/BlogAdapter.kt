c1ffbd7ed954461780e127c28199ed43a5fc7106
ESENS12 <ghdldid12@gmail.com>
2020-08-06 13:07:09 +0900

    Update TIL(Change Adapter to Kotlin and parsing image)

---
+++ SearchNBlogWithoutAds/app/src/main/java/kr/esens/searchnblogwithoutads/BlogAdapter.kt
@@ -0,0 +1,45 @@
+package kr.esens.searchnblogwithoutads
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class BlogAdapter(val mContext: Context, val mData: ArrayList<String>) :
+    RecyclerView.Adapter<BlogAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(mContext).inflate(R.layout.blog_listitem, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return mData.size
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(mData,position, mContext)
+    }
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val iv_thumbnail = itemView?.findViewById<ImageView>(R.id.iv_thumbnail)
+        val tv_index = itemView?.findViewById<TextView>(R.id.tv_index)
+
+        fun bind (list: ArrayList<String>, position: Int,context: Context) {
+
+            if (iv_thumbnail != null) {
+                Glide.with(context)
+                    .load(list[position])
+                    //                    .override(이미지 사이즈) //만약 리소스 조정이 필요하다면, 캐시낭비를 막는데 효과적임
+                    //                .placeholder(R.drawable.loading)
+                    //                .error(R.drawable.image_not_found)
+                    .into(iv_thumbnail)
+            }
+            tv_index?.text = "$position"
+
+        }
+    }
+}
--- SearchNBlogWithoutAds/app/src/main/java/kr/esens/searchnblogwithoutads/MainActivity.kt
+++ SearchNBlogWithoutAds/app/src/main/java/kr/esens/searchnblogwithoutads/MainActivity.kt
@@ -1,11 +1,13 @@
 package kr.esens.searchnblogwithoutads
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.webkit.WebViewClient
 import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -24,68 +26,82 @@ import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         const val TAG = "MainActivity"
+        private lateinit var mContext : Context;
     }
 
     private val nBlog_img_tag = "div.se-module.se-module-image > a > img[src]" // naver blog 이미지 태그
     private val parentJob = Job()
     private val coroutineScope = CoroutineScope(Dispatchers.Main + parentJob)
     private var ar_blogList = ArrayList<String>();
-    private var blogAdapter : BlogAdapter? = null;
+    private var blogAdapter: BlogAdapter? = null;
 
 //    private val blogAdapter = BlogAdapter(ar_blogList);
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mContext = this.applicationContext;
         my_webview.webViewClient = WebViewClient()
-//        cl_no_data.visibility = View.INVISIBLE;
 
-        blogAdapter = BlogAdapter(ar_blogList);
+        blogAdapter = BlogAdapter(mContext, ar_blogList);
 
-        btn_search.setOnClickListener{
-            Log.i(TAG,"Execute Search! : " + et_searchQuery.text);
+        btn_search.setOnClickListener {
+            Log.i(TAG, "Execute Search! : " + et_searchQuery.text);
             executeSearch(et_searchQuery.text.toString())
         }
 
         val mLinearLayoutManager = LinearLayoutManager(this);
+
         recycler_view.apply {
             this.adapter = blogAdapter
             this.layoutManager = mLinearLayoutManager
+            this.addItemDecoration(
+                DividerItemDecoration(
+                    mContext,
+                    LinearLayoutManager.HORIZONTAL
+                )
+            )
         }
     }
 
-    fun executeSearch(searchQuery : String){
+    fun executeSearch(searchQuery: String) {
 
         var searchOption = "sim";
         var page = 1;
         cl_no_data.visibility = View.GONE;
 
-        SearchRetrofit.getService().requestBlogList(query = searchQuery, st = searchOption, start = page).enqueue(object :
+        SearchRetrofit.getService()
+            .requestBlogList(query = searchQuery, st = searchOption, start = page).enqueue(object :
             Callback<ResponseBody> {
             override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
                 Log.e(TAG, "res Failure : $t");
             }
 
+                //todo Filtering Ads Classifier, UI Design
+
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
-                val str_res : String = response.body()?.string().toString();
+                val str_res: String = response.body()?.string().toString();
 
                 coroutineScope.launch(Dispatchers.IO) {
-                    val res : Document? = Jsoup.parse(str_res)
+                    val res: Document? = Jsoup.parse(str_res)
                     val elem: Elements? = res?.select("a.sh_blog_title")
                     elem?.forEachIndexed() { index, it ->
 
+                        //블로그 url 추출
                         val blog_url = parseBlogUri(it.attr("href"))
-                        Log.d(TAG,"blog_url : $blog_url")
+                        Log.d(TAG, "blog_url : $blog_url")
 
-                        val elem_item : Elements? = getImageUriFromBlog(blog_url);
+                        //해당 블로그에서 이미지 태그 uri 추출
+                        val elem_item: Elements? = getImageUriFromBlog(blog_url);
 
                         elem_item?.forEach {
-//                            Log.d(TAG,"img : ${it.attr("src")}");
-                            ar_blogList.add(it.attr("src"));
+                            val str_res = it.attr("src").replace("w80_blur" , "w800");
+
+                            ar_blogList.add(str_res);
                         }
-                        Log.d(TAG,"====================$index=======================")
+                        Log.d(TAG, "====================$index=======================")
                     }
                 }
                 blogAdapter?.notifyDataSetChanged();
@@ -94,14 +110,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     @UiThread
-    private fun getImageUriFromBlog(url : String): Elements? {
-        val response : Connection.Response;
+    private fun getImageUriFromBlog(url: String): Elements? {
+        val response: Connection.Response;
 
         try {
             response = Jsoup.connect(url)
                 .method(Connection.Method.GET)
                 .execute()
-        }catch(e : Exception){
+        } catch (e: Exception) {
             e.printStackTrace();
             Log.e(TAG, "ERROR  : $e");
             return null
@@ -110,7 +126,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     //blog.me로 끝나는 naver blog url을 blog.naver.com으로 변경해줘야함
-    private fun parseBlogUri(url : String) : String {
+    private fun parseBlogUri(url: String): String {
         if (url.indexOf("blog.me") > -1) {
             val nickName = url.substring(url.indexOf("/") + 2, url.indexOf("."))
             val postNumber = url.substring(url.lastIndexOf("/") + 1)
