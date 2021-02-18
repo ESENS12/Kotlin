99b95c6509a7475301ba6e70f7cc67fbabff05b4
ESENS12 <ghdldid12@gmail.com>
2021-02-18 10:51:43 +0900

    add basic camera call sample

--- CameraSample/app/src/main/java/biz/fatos/auto/camerasample/MainActivity.kt
+++ CameraSample/app/src/main/java/biz/fatos/auto/camerasample/MainActivity.kt
@@ -1,13 +1,79 @@
 package biz.fatos.auto.camerasample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    val REQUEST_IMAGE_CAPTURE = 1
+    val PERMISSION_REQUEST_CODE = 200
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        if(checkPermission()){
+            dispatchTakePictureIntent();
+        }else{
+            requestPermission();
+        }
+    }
+
+
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            false
+        } else true
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),
+                PERMISSION_REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> if (grantResults.isNotEmpty() && grantResults[0] === PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent();
+            } else {
+                finishAffinity();
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+
+        val builder = AlertDialog.Builder(this);
+        builder.setTitle("앱 종료")
+        builder.setMessage("종료 하시겠습니까?")
+        builder.setPositiveButton("예") { _,_ -> finishAffinity() }
+        builder.setNegativeButton("취소") { dialogInterface, _ -> dialogInterface.dismiss()}
+        builder.show();
+
+//        super.onBackPressed()
     }
 }
