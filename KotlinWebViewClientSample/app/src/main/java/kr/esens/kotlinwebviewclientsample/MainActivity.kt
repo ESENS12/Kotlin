c8305c20219d87e574227f4b2d39d5baae84664b
ESENS12 <ghdldid12@gmail.com>
2021-12-20 12:23:04 +0900

    Add WebViewClientSample

---
+++ KotlinWebViewClientSample/app/src/main/java/kr/esens/kotlinwebviewclientsample/MainActivity.kt
@@ -0,0 +1,244 @@
+package kr.esens.kotlinwebviewclientsample
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.http.SslError
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.view.KeyEvent
+import android.webkit.*
+import android.widget.Toast
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+    lateinit var mContext : Context
+    private val STR_URL = ""
+    companion object {
+        lateinit var mWebView : WebView
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mWebView = findViewById(R.id.myWebView);
+        mContext = this
+        requestPermission()
+
+        var myChromeClient = MyChromeClient()
+        myChromeClient.mContext = mContext;
+
+        mWebView.apply {
+            webViewClient = myWebViewClient()
+            webChromeClient = myChromeClient
+            settings.javaScriptEnabled = true
+            settings.userAgentString = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19"
+            settings.setSupportMultipleWindows(false)
+        }
+
+    }
+
+    private fun loadWebView(){
+
+        if(mWebView != null){
+            mWebView.loadUrl(STR_URL)
+        }
+    }
+
+    inner class myWebViewClient : WebViewClient(){
+        override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+            return super.shouldOverrideUrlLoading(view, url)
+        }
+
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+        }
+
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+        }
+
+        override fun onLoadResource(view: WebView?, url: String?) {
+            super.onLoadResource(view, url)
+        }
+
+        override fun onPageCommitVisible(view: WebView?, url: String?) {
+            super.onPageCommitVisible(view, url)
+        }
+
+        override fun shouldInterceptRequest(view: WebView?, url: String?): WebResourceResponse? {
+            return super.shouldInterceptRequest(view, url)
+        }
+
+        override fun shouldInterceptRequest(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): WebResourceResponse? {
+            return super.shouldInterceptRequest(view, request)
+        }
+
+        override fun onTooManyRedirects(
+            view: WebView?,
+            cancelMsg: Message?,
+            continueMsg: Message?
+        ) {
+            super.onTooManyRedirects(view, cancelMsg, continueMsg)
+        }
+
+        override fun onReceivedError(
+            view: WebView?,
+            errorCode: Int,
+            description: String?,
+            failingUrl: String?
+        ) {
+            super.onReceivedError(view, errorCode, description, failingUrl)
+        }
+
+        override fun onReceivedError(
+            view: WebView?,
+            request: WebResourceRequest?,
+            error: WebResourceError?
+        ) {
+            super.onReceivedError(view, request, error)
+        }
+
+        override fun onReceivedHttpError(
+            view: WebView?,
+            request: WebResourceRequest?,
+            errorResponse: WebResourceResponse?
+        ) {
+            super.onReceivedHttpError(view, request, errorResponse)
+        }
+
+        override fun onFormResubmission(view: WebView?, dontResend: Message?, resend: Message?) {
+            super.onFormResubmission(view, dontResend, resend)
+        }
+
+        override fun doUpdateVisitedHistory(view: WebView?, url: String?, isReload: Boolean) {
+            super.doUpdateVisitedHistory(view, url, isReload)
+        }
+
+        override fun onReceivedSslError(
+            view: WebView?,
+            handler: SslErrorHandler?,
+            error: SslError?
+        ) {
+            super.onReceivedSslError(view, handler, error)
+        }
+
+        override fun onReceivedClientCertRequest(view: WebView?, request: ClientCertRequest?) {
+            super.onReceivedClientCertRequest(view, request)
+        }
+
+        override fun onReceivedHttpAuthRequest(
+            view: WebView?,
+            handler: HttpAuthHandler?,
+            host: String?,
+            realm: String?
+        ) {
+            super.onReceivedHttpAuthRequest(view, handler, host, realm)
+        }
+
+        override fun shouldOverrideKeyEvent(view: WebView?, event: KeyEvent?): Boolean {
+            return super.shouldOverrideKeyEvent(view, event)
+        }
+
+        override fun onUnhandledKeyEvent(view: WebView?, event: KeyEvent?) {
+            super.onUnhandledKeyEvent(view, event)
+        }
+
+        override fun onScaleChanged(view: WebView?, oldScale: Float, newScale: Float) {
+            super.onScaleChanged(view, oldScale, newScale)
+        }
+
+        override fun onReceivedLoginRequest(
+            view: WebView?,
+            realm: String?,
+            account: String?,
+            args: String?
+        ) {
+            super.onReceivedLoginRequest(view, realm, account, args)
+        }
+
+        override fun onRenderProcessGone(
+            view: WebView?,
+            detail: RenderProcessGoneDetail?
+        ): Boolean {
+            return super.onRenderProcessGone(view, detail)
+        }
+
+        override fun onSafeBrowsingHit(
+            view: WebView?,
+            request: WebResourceRequest?,
+            threatType: Int,
+            callback: SafeBrowsingResponse?
+        ) {
+            super.onSafeBrowsingHit(view, request, threatType, callback)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        var bAllGranted = true
+        if (requestCode == 1000) {
+            for (i in grantResults.indices) {
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        this,
+                        "require permissions denied",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    finishAffinity()
+                    bAllGranted = false
+                    break
+                }
+            }
+
+            if (bAllGranted) {
+                try {
+                    loadWebView()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    fun requestPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val pInternet: Int = checkSelfPermission(Manifest.permission.INTERNET)
+
+            if (pInternet == PackageManager.PERMISSION_DENIED) {
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.INTERNET
+                    ), 1000
+                )
+            } else {
+                loadWebView()
+            }
+        } else {
+
+            try {
+                loadWebView()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
---
+++ KotlinWebViewClientSample/app/src/main/java/kr/esens/kotlinwebviewclientsample/MyChromeClient.kt
@@ -0,0 +1,216 @@
+package kr.esens.kotlinwebviewclientsample
+
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Message
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.*
+
+class MyChromeClient : WebChromeClient() {
+
+    private val TAG = "MyChromeClient"
+    lateinit var mContext : Context
+
+    override fun onProgressChanged(view: WebView?, newProgress: Int) {
+        Log.d(TAG,"onProgressChanged called : ${newProgress}")
+        super.onProgressChanged(view, newProgress)
+    }
+
+    override fun onReceivedTitle(view: WebView?, title: String?) {
+        Log.d(TAG,"onReceivedTitle called : ${title}")
+        super.onReceivedTitle(view, title)
+    }
+
+    override fun onReceivedIcon(view: WebView?, icon: Bitmap?) {
+        Log.d(TAG,"onReceivedIcon called : ${icon.toString()}")
+        super.onReceivedIcon(view, icon)
+    }
+
+    override fun onReceivedTouchIconUrl(view: WebView?, url: String?, precomposed: Boolean) {
+        Log.d(TAG,"onReceivedTouchIconUrl called : ${url}")
+        super.onReceivedTouchIconUrl(view, url, precomposed)
+    }
+
+    override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+        Log.d(TAG,"onReceivedTouchIconUrl called")
+        super.onShowCustomView(view, callback)
+    }
+
+    override fun onShowCustomView(
+        view: View?,
+        requestedOrientation: Int,
+        callback: CustomViewCallback?
+    ) {
+        super.onShowCustomView(view, requestedOrientation, callback)
+    }
+
+    override fun onHideCustomView() {
+        Log.d(TAG,"onHideCustomView called")
+        super.onHideCustomView()
+    }
+
+    override fun onCreateWindow(
+        view: WebView?,
+        isDialog: Boolean,
+        isUserGesture: Boolean,
+        resultMsg: Message?
+    ): Boolean {
+        Log.d(TAG,"onCreateWindow called")
+//        val newWebView = WebView(MainActivity@mContext).apply {
+//            settings.javaScriptEnabled = true
+//        }
+
+//        val dialog = Dialog(MainActivity@mContext).apply {
+//            setContentView(view!!)
+//            window!!.attributes.width = ViewGroup.LayoutParams.WRAP_CONTENT
+//            window!!.attributes.height = ViewGroup.LayoutParams.WRAP_CONTENT
+//            show()
+//        }
+//        newWebView.webChromeClient = object : WebChromeClient(){
+//            override fun onCloseWindow(window: WebView?) {
+//                dialog.dismiss()
+//            }
+//        }
+
+        (resultMsg?.obj as WebView.WebViewTransport).webView = view
+        resultMsg.sendToTarget()
+        return true
+//        return super.onCreateWindow(view, isDialog, isUserGesture, resultMsg)
+    }
+
+    override fun onRequestFocus(view: WebView?) {
+        Log.d(TAG,"onRequestFocus called")
+        super.onRequestFocus(view)
+    }
+
+    override fun onCloseWindow(window: WebView?) {
+        Log.d(TAG,"onCloseWindow called")
+        super.onCloseWindow(window)
+    }
+
+    override fun onJsAlert(
+        view: WebView?,
+        url: String?,
+        message: String?,
+        result: JsResult?
+    ): Boolean {
+        Log.d(TAG,"onJsAlert called url : $url , msg : $message")
+        return super.onJsAlert(view, url, message, result)
+    }
+
+    override fun onJsConfirm(
+        view: WebView?,
+        url: String?,
+        message: String?,
+        result: JsResult?
+    ): Boolean {
+        Log.d(TAG,"onJsConfirm called url : $url , msg : $message")
+        return super.onJsConfirm(view, url, message, result)
+    }
+
+    override fun onJsPrompt(
+        view: WebView?,
+        url: String?,
+        message: String?,
+        defaultValue: String?,
+        result: JsPromptResult?
+    ): Boolean {
+        Log.d(TAG,"onJsConfirm called url : $url , msg : $message")
+        return super.onJsPrompt(view, url, message, defaultValue, result)
+    }
+
+    override fun onJsBeforeUnload(
+        view: WebView?,
+        url: String?,
+        message: String?,
+        result: JsResult?
+    ): Boolean {
+        Log.d(TAG,"onJsBeforeUnload called url : $url , msg : $message")
+        return super.onJsBeforeUnload(view, url, message, result)
+    }
+
+    override fun onExceededDatabaseQuota(
+        url: String?,
+        databaseIdentifier: String?,
+        quota: Long,
+        estimatedDatabaseSize: Long,
+        totalQuota: Long,
+        quotaUpdater: WebStorage.QuotaUpdater?
+    ) {
+        super.onExceededDatabaseQuota(
+            url,
+            databaseIdentifier,
+            quota,
+            estimatedDatabaseSize,
+            totalQuota,
+            quotaUpdater
+        )
+    }
+
+    override fun onReachedMaxAppCacheSize(
+        requiredStorage: Long,
+        quota: Long,
+        quotaUpdater: WebStorage.QuotaUpdater?
+    ) {
+        super.onReachedMaxAppCacheSize(requiredStorage, quota, quotaUpdater)
+    }
+
+    override fun onGeolocationPermissionsShowPrompt(
+        origin: String?,
+        callback: GeolocationPermissions.Callback?
+    ) {
+        super.onGeolocationPermissionsShowPrompt(origin, callback)
+    }
+
+    override fun onGeolocationPermissionsHidePrompt() {
+        super.onGeolocationPermissionsHidePrompt()
+    }
+
+    override fun onPermissionRequest(request: PermissionRequest?) {
+        Log.d(TAG,"onPermissionRequest called request : ${request.toString()}")
+
+        super.onPermissionRequest(request)
+    }
+
+    override fun onPermissionRequestCanceled(request: PermissionRequest?) {
+        super.onPermissionRequestCanceled(request)
+    }
+
+    override fun onJsTimeout(): Boolean {
+        return super.onJsTimeout()
+    }
+
+    override fun onConsoleMessage(message: String?, lineNumber: Int, sourceID: String?) {
+        super.onConsoleMessage(message, lineNumber, sourceID)
+    }
+
+    override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+
+        Log.e(TAG,"onConsoleMessage called msg : ${consoleMessage?.message()}")
+        return super.onConsoleMessage(consoleMessage)
+    }
+
+    override fun getDefaultVideoPoster(): Bitmap? {
+        return super.getDefaultVideoPoster()
+    }
+
+    override fun getVideoLoadingProgressView(): View? {
+        return super.getVideoLoadingProgressView()
+    }
+
+    override fun getVisitedHistory(callback: ValueCallback<Array<String>>?) {
+        super.getVisitedHistory(callback)
+    }
+
+    override fun onShowFileChooser(
+        webView: WebView?,
+        filePathCallback: ValueCallback<Array<Uri>>?,
+        fileChooserParams: FileChooserParams?
+    ): Boolean {
+        return super.onShowFileChooser(webView, filePathCallback, fileChooserParams)
+    }
+}
