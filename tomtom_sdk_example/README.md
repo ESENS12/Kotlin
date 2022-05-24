b33b9aad4b5d97f7201586671071b90d4ca2106f
ESENS12 <ghdldid12@gmail.com>
2022-05-24 13:56:55 +0900

    Add BaseMap Example

--- tomtom_sdk_example/README.md
+++ tomtom_sdk_example/README.md
@@ -3,3 +3,28 @@
 
 # References
  - https://developer.tomtom.com/maps-android-sdk/documentation/product-information/introduction
+
+# Notes
+Key valid check url
+https://api.tomtom.com/map/1/tile/basic/main/0/0/0.png?key=<YOUR_SDK_KEY>
+
+빌드 오류
+
+1. Annotation 관련
+
+implementation "com.android.support:support-annotations:28.0.0"
+App 레벨 build.gradle에 추가.
+
+2. 다중 manifest 파일 병합 처리 필요
+   AndroidManifest 에 tools:node="replace" 추가
+
+3. 가장 어이없는 버전 문제 . 2.4.807 로 써놓고 안된다고 계속 오류 찾았는데 사실 2.4807 형태로 해야함
+   implementation "com.tomtom.online:sdk-maps:2.4807"
+   릴리즈 노트에 2.4.807 형태로 표기되어있음……
+   https://developer.tomtom.com/maps-android-sdk/documentation/product-information/release-notes
+
+4. AndroidX 관련 오류
+   gradle.properties 에
+   android.enableJetifier=true 추가.
+
+5. 현재 map tile 못받아오는 문제 있음. android 21 저장소 문제인지 권한문제인지 확인 후 수정해야함 
--- tomtom_sdk_example/app/src/main/java/kr/esens/MainActivity.kt
+++ tomtom_sdk_example/app/src/main/java/kr/esens/MainActivity.kt
@@ -1,11 +1,58 @@
 package kr.esens
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.tomtom.online.sdk.common.location.LatLng
+import com.tomtom.online.sdk.common.permission.AppPermissionHandler
+import com.tomtom.online.sdk.map.*
+import com.tomtom.online.sdk.map.model.MapTilesType
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, TomtomMapCallback.OnMapLongClickListener {
+    val TAG = "MainActivity"
+    lateinit var tomMap: TomtomMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initLocationPermissions()
+        initTomTomServices()
     }
+
+    override fun onMapReady(tomtomMap: TomtomMap) {
+        Log.e(TAG,"onMapReady Called!")
+        this.tomMap = tomtomMap;
+
+        tomMap.setMyLocationEnabled(true);
+        tomMap.addOnMapLongClickListener(this);
+        tomMap.getMarkerSettings().setMarkersClustering(true)
+    }
+
+    private fun initLocationPermissions() {
+        val permissionHandler = AppPermissionHandler(this)
+        permissionHandler.addLocationChecker()
+        permissionHandler.askForNotGrantedPermissions()
+    }
+
+
+    override fun onMapLongClick(latLng: LatLng) {
+        Log.e(TAG,"onMapLongClick called!");
+    }
+
+    private fun initTomTomServices() {
+        val mapKeys = mapOf(
+            ApiKeyType.MAPS_API_KEY to BuildConfig.MAPS_API_KEY
+        )
+        val mapProperties = MapProperties.Builder()
+            .keys(mapKeys)
+            .build()
+        val mapFragment = MapFragment.newInstance(mapProperties)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.map_fragment, mapFragment)
+            .commit()
+        mapFragment.getAsyncMap(this)
+    }
+
 }
